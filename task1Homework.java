f49d122f227a40c4eb9474587445e777316d8e98
Daniil Kobzar <daniilkobzar@MacBook-Pro-petr.local>
2022-06-20 20:00:53 +0200

    homework task1 modified

--- task1Homework.java
+++ task1Homework.java
@@ -1,18 +1,23 @@
 package HomeworkJava;
     // Написать программу вычисления n-ого треугольного числа. T=n(n+1)/2
+import java.util.List;
+import java.util.ArrayList;
 
 public class task1Homework {
     public static void main(String[] args) {
         nNumber(10);
     }
     public static void nNumber(int n) {
-        for (int i = 0; i <= n; i++) {
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
             int t = i*(i+1)/2;
-            for (int j = 0; j <= t; j++) {
-                System.out.print(j);
+            integers.add(t);
+            for (int value : integers) {
+                System.out.print(value + " ");
             }
             System.out.println();
+        }
             
         }
     }
-}
+
