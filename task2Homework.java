4f14be0031f138a1f3cca33b0293114389b26756
Daniil Kobzar <daniilkobzar@MacBook-Pro-petr.local>
2022-06-20 20:32:17 +0200

    Task2 homework modified

--- task2Homework.java
+++ task2Homework.java
@@ -7,15 +7,22 @@ public class task2Homework {
     public static int whatNumber() {
         Random rand = new Random();
         int number = rand.nextInt(100);
-        int digit1 = number / 10;
-        int digit2 = number % 10;
-        int sum = digit1 + digit2;
+        int number1 = number;
+        int length = String.valueOf(number).length();
+        int[] array = new int[length];
+        int sum = 0;
         int res = 0;
-        if (number % sum == 0) {
-            res = number;
+        for (int i = 0; i < array.length; i ++) {
+           int digit = number % 10;
+           array[i] = digit;
+           sum = sum + array[i];
+           number = number / 10;
+        }
+        if (number1 % sum == 0) {
+            res = number1;
         } 
         else {
-            System.out.println("Число " + number + " не делится на сумму их цифр без остатка");
+            System.out.println("Число " + number1 + " не делится на сумму их цифр без остатка");
         }
         return res;
     }
